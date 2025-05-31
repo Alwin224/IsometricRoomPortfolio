efd7552b63531238b0ce0df0edf8e345f3484a7b
Alwin Bhogal <alwinbhogal@outlook.com>
2025-05-30 19:27:52 -0500

    Added a model.js file and set some comments as to where the specific types of variables are going to go in the scene to load the model

---
+++ model.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+//scene
+const scene = new THREE.Scene();
+
+
+//sizes
+const sizes = {
+    width:800,
+    height:800
+}
+
+
+//light
+
+//camera
+
+
+//model from blender
+
+
+//canvas 
+const canvas = document.querySelector('canvas.webgl')
+
+
+//renderer
+
+
