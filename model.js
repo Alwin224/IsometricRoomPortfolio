2507b0fcd374444f2ef3bfb3b978cc7c19ed9886
Alwin224 <alwinbhogal2@gmail.com>
2025-05-31 00:55:57 -0500

    Edited the scene and fixed the html document by adding a canvas section. Also added the camera, renderer, hemispherelight, and canvas

--- model.js
+++ model.js
@@ -8,14 +8,18 @@ const scene = new THREE.Scene();
 //sizes
 const sizes = {
     width:800,
-    height:800
+    height:600
 }
 
 
-//light
+//hemisphere light so that there is a light that points from the top
+const hemisphereLight = new THREE.HemisphereLight(0xffffff)
+scene.add(hemisphereLight)
 
 //camera
-
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 2
+scene.add(camera)
 
 //model from blender
 
@@ -23,7 +27,10 @@ const sizes = {
 //canvas 
 const canvas = document.querySelector('canvas.webgl')
 
-
 //renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
 
-
+renderer.render(scene, camera)
